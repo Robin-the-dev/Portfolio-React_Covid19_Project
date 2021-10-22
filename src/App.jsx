5d5e39f04de38bf1820c01a1c3305cbe137686c7
Robin Hanjun Kwon <khj02021@gmail.com>
2021-10-22 16:37:18 +0900

    Create Contents component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Contents from './components/Contents';
 
 const App = () => {
 	return (
 		<div>
 			<Header />
+			<Contents />
 		</div>
 	);
 }
