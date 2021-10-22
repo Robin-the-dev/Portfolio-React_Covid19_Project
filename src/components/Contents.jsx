1a574bc881dd8dfd2e999568e7226d0b81f6f96b
Robin Hanjun Kwon <khj02021@gmail.com>
2021-10-22 20:27:54 +0900

    Install chart.js and react-chartjs-2

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,17 +1,58 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import {Bar, Doughnut, Line} from 'react-chartjs-2';
 
 const StyledSection = styled.section`
 	padding: 1rem;
 `;
 
 const Contents = () => {
+	const [confirmedData, setConfirmedData] = useState({
+		labels: ["January", "Feburary", "March"],
+		datasets: [
+			{
+				label: "Confirmed cases",
+				backgroundColor: "salmon",
+				fill: true,
+				data: [10, 5, 3]
+			},
+		]
+	});
+
+	useEffect(() => {
+		const makeData = (items) => {
+			items.forEach(item => console.log(item));
+		}
+
+		const fetchEvents = async () => {
+			const res = await axios.get('https://api.covid19api.com/total/dayone/country/kr');
+			makeData(res.data);
+		}
+		fetchEvents();
+	});
+
+	const options = {
+		plugins: {
+			title: {
+				display: true,
+				text: '누적 확진자 통계',
+				fontSize: 16
+			},
+			legend: {
+				display: true,
+				position: 'bottom'
+			}
+		}
+	}
+
 	return (
 		<StyledSection>
 			<h2>국내 코로나 현황</h2>
 			<div>
-				
+				<div>
+					<Bar data={confirmedData} options={options} />
+				</div>
 			</div>
 		</StyledSection>
 	);
