1e6e863748d728e640c18d3478e4557aaf37f8c0
Robin Hanjun Kwon <khj02021@gmail.com>
2021-11-08 14:37:26 +0900

    Finish refactoring

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,13 @@ import {Row, Col} from 'react-bootstrap';
 
 import MyCard from './MyCard';
 
+const StyledDiv = styled.div`
+	padding-top: 3rem;
+	padding-bottom: 3rem;
+`;
+
 const StyledH2 = styled.h2`
 	text-align: center;
-	margin-top: 3rem;
 	margin-bottom: 2rem;
 `;
 
@@ -33,23 +37,39 @@ const Home = () => {
 	}, []);
 
 	return (
-		<div>
+		<StyledDiv>
 			<StyledH2>Global Covid-19 Status in {globalCase.currentDate}</StyledH2>
 			<Row lg={4} md={2} sm={1} xs={1}>
 				<Col>
-					<MyCard header='Total Confirmed Cases' title={globalCase.totalConfirmed} text='It is blah' />
+					<MyCard
+						header='Total Confirmed Cases'
+						title={globalCase.totalConfirmed}
+						text='It is total confirmed cases.'
+					/>
 				</Col>
 				<Col>
-					<MyCard header='New Confirmed Cases' title={globalCase.newConfirmed} text='It is blah' />
+					<MyCard
+						header='New Confirmed Cases'
+						title={globalCase.newConfirmed}
+						text='It is new confirmed cases.'
+					/>
 				</Col>
 				<Col>
-					<MyCard header='Total Death Cases' title={globalCase.totalDeaths} text='It is blah' />
+					<MyCard
+						header='Total Death Cases'
+						title={globalCase.totalDeaths}
+						text='It is total death cases.'
+					/>
 				</Col>
 				<Col>
-					<MyCard header='New Death Cases' title={globalCase.newDeaths} text='It is blah' />
+					<MyCard
+						header='New Death Cases'
+						title={globalCase.newDeaths}
+						text='It is new death cases'
+					/>
 				</Col>
 			</Row>
-		</div>
+		</StyledDiv>
 	);
 }
 
